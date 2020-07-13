a31bf6ffbab68adc5341f820cbdd89a16e504981
Choibumsu <bumsu0211@gmail.com>
2020-07-13 15:18:48 +0900

    [#1] feat(setting) - 프로젝트 세팅
    npm 패키지 설치
    폴더구조 설정
    babel, webpack 설정

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  entry: './public/javascripts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+}
