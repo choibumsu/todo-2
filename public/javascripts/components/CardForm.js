65f8492e34a5639470367ccb65590cff0d9a5c33
Choibumsu <bumsu0211@gmail.com>
2020-07-15 17:28:19 +0900

    [#26] feat(card form) - 카드 폼 컴포넌트 생성

--- public/javascripts/components/CardForm.js
+++ public/javascripts/components/CardForm.js
@@ -1,1 +1,32 @@
+import { templateToElement } from '../utils/HtmlGenerator'
 import '../../stylesheets/components/cardForm.scss'
+
+export default class CardForm {
+  constructor() {
+    this.$target = ''
+
+    this.init()
+  }
+
+  init() {
+    this.setTarget()
+  }
+
+  setTarget() {
+    const template = `
+      <div class='card-form'>
+        <textarea class='card-textarea' placeholder='Enter a note' maxlength='500'></textarea>
+        <div class='button-row'>
+          <div class='btn-wrapper'>
+            <div class='btn add-btn unactive'>Add</div>
+          </div>
+          <div class='btn-wrapper'>
+            <div class='btn cancel-btn'>Cancel</div>
+          </div>
+        </div>
+      </div>
+    `
+
+    this.$target = templateToElement(template)
+  }
+}
