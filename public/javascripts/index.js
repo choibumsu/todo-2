23db37602ae80e1b466f26661cfaa97e9fca9307
Choibumsu <bumsu0211@gmail.com>
2020-07-15 14:44:46 +0900

    [#25] refactor(setting) - 웹팩 설정 리팩토링
    자바스크립트 웹팩 재설정 : public/javascripts/index.js 를 시작파일로 설정
    scss 웹팩 빌드 : mini-css-extract-plugin, css-loader, scss-loader로 빌드

---
+++ public/javascripts/index.js
@@ -0,0 +1,14 @@
+import { closeModal } from './components/Modal'
+import { toggleSidebar } from './components/SideBar'
+import './components/Card'
+import './components/CardForm'
+import './components/Column'
+import './components/DeleteCardModal'
+import './components/EditCardModal'
+import './components/EditColumnModal'
+import './components/Header'
+import './components/LoginForm'
+import './components/SideBarCard'
+import '../stylesheets/common/base.scss'
+
+console.log('hi')
