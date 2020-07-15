6967928c75b8b439f49c855ca266bde514f13ec6
Choibumsu <bumsu0211@gmail.com>
2020-07-15 14:52:04 +0900

    [#25] refactor(webpack) - index.js에 추가 및 JS개발을 위해 파일을 생성

--- public/javascripts/components/SideBar.js
+++ public/javascripts/components/SideBar.js
@@ -1,10 +1,11 @@
 import { CLASS_NAME } from '../utils/Constants.js'
+import '../../stylesheets/components/sidebar.scss'
 
 const menu = document.querySelector('#menu')
 const sidebar = document.querySelector('#sidebar')
 menu.addEventListener('click', toggleSidebar)
 
-function toggleSidebar() {
+export function toggleSidebar() {
   if (sidebar.classList.contains(CLASS_NAME.DP_NONE)) {
     sidebar.classList.remove(CLASS_NAME.DP_NONE)
     return
