f64b929a872e4ee4284d1d4660d318197f0c80cf
Choibumsu <bumsu0211@gmail.com>
2020-07-16 12:58:05 +0900

    [#26] refactor(card form) - CardForm 컴포넌트 메소드 프로토타입으로 이동

--- public/javascripts/components/CardForm.js
+++ public/javascripts/components/CardForm.js
@@ -1,7 +1,13 @@
-import { templateToElement } from '../utils/HtmlGenerator'
-import { CLASS_NAME, CARD_FORM_CLASS, COLUMN_CLASS } from '../utils/Constants'
-import '../../stylesheets/components/cardForm.scss'
 import Card from './Card'
+import { templateToElement } from '../utils/HtmlGenerator'
+import {
+  CLASS_NAME,
+  CARD_FORM_CLASS,
+  COLUMN_CLASS,
+  EVENT,
+} from '../utils/Constants'
+import { emitter } from '../utils/EventEmitter'
+import '../../stylesheets/components/cardForm.scss'
 
 export default class CardForm {
   constructor() {
@@ -13,79 +19,78 @@ export default class CardForm {
 
     this.init()
   }
+}
 
-  init() {
-    this.setElements()
-    this.bindEvent()
-  }
+CardForm.prototype.init = function () {
+  this.setElements()
+  this.bindEvent()
+}
 
-  setElements() {
-    const template = `
-      <div class='card-form'>
-        <textarea class='card-textarea' placeholder='Enter a note' maxlength='500'></textarea>
-        <div class='button-row'>
-          <div class='btn-wrapper'>
-            <div class='btn add-btn unactive'>Add</div>
-          </div>
-          <div class='btn-wrapper'>
-            <div class='btn cancel-btn'>Cancel</div>
-          </div>
+CardForm.prototype.setElements = function () {
+  const template = `
+    <div class='card-form'>
+      <textarea class='card-textarea' placeholder='Enter a note' maxlength='500'></textarea>
+      <div class='button-row'>
+        <div class='btn-wrapper'>
+          <div class='btn add-btn unactive'>Add</div>
+        </div>
+        <div class='btn-wrapper'>
+          <div class='btn cancel-btn'>Cancel</div>
         </div>
       </div>
-    `
+    </div>
+  `
 
-    this.$target = templateToElement(template)
-    this.$cardTextarea = this.$target.querySelector(
-      `.${CARD_FORM_CLASS.TEXTAREA}`
-    )
-    this.$addBtn = this.$target.querySelector(`.${CARD_FORM_CLASS.ADD_BTN}`)
-    this.$cancelBtn = this.$target.querySelector(
-      `.${CARD_FORM_CLASS.CANCEL_BTN}`
-    )
+  this.$target = templateToElement(template)
+  this.$cardTextarea = this.$target.querySelector(
+    `.${CARD_FORM_CLASS.TEXTAREA}`
+  )
+  this.$addBtn = this.$target.querySelector(`.${CARD_FORM_CLASS.ADD_BTN}`)
+  this.$cancelBtn = this.$target.querySelector(`.${CARD_FORM_CLASS.CANCEL_BTN}`)
+}
+
+CardForm.prototype.bindEvent = function () {
+  this.$cardTextarea.addEventListener('input', (e) => {
+    this.setActive(e.target.value)
+  })
+
+  this.$cancelBtn.addEventListener('click', (e) => {
+    this.removeCardForm()
+  })
+
+  this.$addBtn.addEventListener('click', (e) => {
+    this.addCard()
+  })
+}
+
+CardForm.prototype.setActive = function (isActive) {
+  this.isActive = isActive
+
+  if (this.isActive) {
+    this.$addBtn.classList.remove(CLASS_NAME.UNACTIVE)
+    return
   }
 
-  bindEvent() {
-    this.$cardTextarea.addEventListener('input', (e) => {
-      this.setActive(e.target.value)
-    })
+  this.$addBtn.classList.add(CLASS_NAME.UNACTIVE)
+}
 
-    this.$cancelBtn.addEventListener('click', (e) => {
-      this.removeCardForm()
-    })
+CardForm.prototype.removeCardForm = function () {
+  this.$target.remove()
+}
 
-    this.$addBtn.addEventListener('click', (e) => {
-      this.addCard()
-    })
+CardForm.prototype.addCard = function () {
+  if (!this.isActive) {
+    return
   }
 
-  setActive(isActive) {
-    this.isActive = isActive
+  const $parentColumn = this.$target.closest('.column')
+  const $contentContainer = $parentColumn.querySelector(
+    `.${COLUMN_CLASS.CONTENT_CONTAINER}`
+  )
+  const $newCard = new Card(this.$cardTextarea.value).getTarget()
+  $contentContainer.prepend($newCard)
+  emitter.emit(EVENT.CHANGE_CARD_COUNT)
 
-    if (this.isActive) {
-      this.$addBtn.classList.remove(CLASS_NAME.UNACTIVE)
-      return
-    }
-
-    this.$addBtn.classList.add(CLASS_NAME.UNACTIVE)
-  }
-
-  removeCardForm() {
-    this.$target.remove()
-  }
-
-  addCard() {
-    if (!this.isActive) {
-      return
-    }
-
-    const $parentColumn = this.$target.closest('.column')
-    const $contentContainer = $parentColumn.querySelector(
-      `.${COLUMN_CLASS.CONTENT_CONTAINER}`
-    )
-    const $newCard = new Card(this.$cardTextarea.value).getTarget()
-    $contentContainer.prepend($newCard)
-
-    this.$cardTextarea.value = ''
-    this.setActive(false)
-  }
+  this.$cardTextarea.value = ''
+  this.setActive(false)
 }
