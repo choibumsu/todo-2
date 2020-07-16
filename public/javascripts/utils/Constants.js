0b53197a3fa54ba290c72221912a36a670af4b1c
Choibumsu <bumsu0211@gmail.com>
2020-07-16 12:45:00 +0900

    [#26] feat(event) - event emitter 생성
    컬럼 컴포넌트의 카드 카운트를 수정하기 위해 event emitter 구현
    카드 폼에서 카드를 생성하면 이벤트를 발생시키고,
    컬럼에서 해당 이벤트를 받으면 카드 카운트를 수정함

--- public/javascripts/utils/Constants.js
+++ public/javascripts/utils/Constants.js
@@ -6,6 +6,7 @@ export const CLASS_NAME = {
 export const COLUMN_CLASS = {
   CONTAINER: 'column-container',
   ADD_BTN: 'add-btn',
+  CARD_COUNT: 'card-count',
   CARD_FORM_SLOT: 'card-form-slot',
   CONTENT_CONTAINER: 'content-container',
 }
@@ -14,4 +15,12 @@ export const CARD_FORM_CLASS = {
   TEXTAREA: 'card-textarea',
   ADD_BTN: 'add-btn',
   CANCEL_BTN: 'cancel-btn',
+}
+
+export const CARD_CLASS = {
+  CARD: 'card',
+}
+
+export const EVENT = {
+  CHANGE_CARD_COUNT: 'change-card-count',
 }
---
+++ public/javascripts/utils/EventEmitter.js
@@ -0,0 +1,15 @@
+class Emitter {
+  on(type, callback) {
+    this['_on' + type] = this['_on' + type] || []
+    this['_on' + type].push(callback)
+  }
+
+  emit(type, args) {
+    this['_on' + type] &&
+      this['_on' + type].forEach((callback) => {
+        callback(args)
+      })
+  }
+}
+
+export const emitter = new Emitter()
