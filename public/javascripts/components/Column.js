d81707d1e5623df574683b4c8101e37d453410d4
Choibumsu <bumsu0211@gmail.com>
2020-07-16 12:56:37 +0900

    [#26] refactor(column) - Column	컴포넌트 메소드 프로토타입으로 이동
    컬럼 컴포넌트 내 메소드 프로토타입으로 이동
    여러 인스턴스가 생겨도 메소드가 중복 생성되는 것 방지

--- public/javascripts/components/Column.js
+++ public/javascripts/components/Column.js
@@ -1,7 +1,8 @@
 import { templateToElement } from '../utils/HtmlGenerator'
 import CardForm from './CardForm'
+import { COLUMN_CLASS, CARD_CLASS, EVENT } from '../utils/Constants'
+import { emitter } from '../utils/EventEmitter'
 import '../../stylesheets/components/column.scss'
-import { COLUMN_CLASS } from '../utils/Constants'
 
 export default class Column {
   constructor({ columnTitle, cardCount }) {
@@ -11,59 +12,68 @@ export default class Column {
 
     this.init()
   }
+}
 
-  init() {
-    this.setElements()
-    this.render()
-    this.bindEvent()
-  }
+Column.prototype.init = function () {
+  this.setElements()
+  this.render()
+  this.bindEvent()
+}
 
-  setElements() {
-    const template = `
-      <section class='column'>
-        <div class='title-bar'>
-          <div class='title-wrapper'>
-            <div class='card-count'>${this.cardCount}</div>
-            <div class='title'>${this.columnTitle}</div> 
-          </div>
-          <div class='btn-wrapper'>
-            <img class='add-btn' src='/static/images/plus-btn.svg' alt='add-btn' />
-            <img class='remove-btn' src='/static/images/remove-btn.svg' alt='remove-btn' />
-          </div>
+Column.prototype.setElements = function () {
+  const template = `
+    <section class='column'>
+      <div class='title-bar'>
+        <div class='title-wrapper'>
+          <div class='card-count'>${this.cardCount}</div>
+          <div class='title'>${this.columnTitle}</div> 
         </div>
-        <div class='card-form-slot'></div>
-        <div class='content-container'></div>
-      </section>
-    `
+        <div class='btn-wrapper'>
+          <img class='add-btn' src='/static/images/plus-btn.svg' alt='add-btn' />
+          <img class='remove-btn' src='/static/images/remove-btn.svg' alt='remove-btn' />
+        </div>
+      </div>
+      <div class='card-form-slot'></div>
+      <div class='content-container'></div>
+    </section>
+  `
 
-    this.$target = templateToElement(template)
+  this.$target = templateToElement(template)
+  this.$cardCount = this.$target.querySelector(`.${COLUMN_CLASS.CARD_COUNT}`)
+}
+
+Column.prototype.render = function () {
+  const $columnContainer = document.querySelector(`.${COLUMN_CLASS.CONTAINER}`)
+  $columnContainer.appendChild(this.$target)
+}
+
+Column.prototype.bindEvent = function () {
+  const $cardAddBtn = this.$target.querySelector(`.${COLUMN_CLASS.ADD_BTN}`)
+  $cardAddBtn.addEventListener('click', () => {
+    this.toggleCardForm()
+  })
+
+  emitter.on(EVENT.CHANGE_CARD_COUNT, () => {
+    this.setCardCount()
+  })
+}
+
+Column.prototype.setCardCount = function () {
+  const newCardCount = this.$target.querySelectorAll(`.${CARD_CLASS.CARD}`)
+    .length
+  this.$cardCount.innerHTML = newCardCount
+}
+
+Column.prototype.toggleCardForm = function () {
+  const $cardFormSlot = this.$target.querySelector(
+    `.${COLUMN_CLASS.CARD_FORM_SLOT}`
+  )
+
+  if ($cardFormSlot.innerHTML) {
+    $cardFormSlot.innerHTML = ''
+    return
   }
 
-  render() {
-    const $columnContainer = document.querySelector(
-      `.${COLUMN_CLASS.CONTAINER}`
-    )
-    $columnContainer.appendChild(this.$target)
-  }
-
-  bindEvent() {
-    const $cardAddBtn = this.$target.querySelector(`.${COLUMN_CLASS.ADD_BTN}`)
-    $cardAddBtn.addEventListener('click', () => {
-      this.toggleCardForm()
-    })
-  }
-
-  toggleCardForm() {
-    const $cardFormSlot = this.$target.querySelector(
-      `.${COLUMN_CLASS.CARD_FORM_SLOT}`
-    )
-
-    if ($cardFormSlot.innerHTML) {
-      $cardFormSlot.innerHTML = ''
-      return
-    }
-
-    const cardForm = new CardForm()
-    $cardFormSlot.appendChild(cardForm.$target)
-  }
+  const cardForm = new CardForm()
+  $cardFormSlot.appendChild(cardForm.$target)
 }
