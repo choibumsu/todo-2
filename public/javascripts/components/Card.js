34098bbb2a2133e0f8f861ca59134c5d66e11c98
Choibumsu <bumsu0211@gmail.com>
2020-07-18 00:45:00 +0900

    [#62] fix(prototype) - 프로토타입으로 작성한 메소드 클래스 내부로 이동
    클래스 사용시 prototype 메소드로 등록하지 않아도
    클래스당 하나의 메소드만 존재하여, 클래스 메소드로 모두 변경

--- public/javascripts/components/Card.js
+++ public/javascripts/components/Card.js
@@ -2,7 +2,7 @@ import { templateToElement } from '../utils/HtmlGenerator'
 import EditCardModal from './Modal/EditCardModal'
 import '../../stylesheets/components/card.scss'
 import DeleteCardModal from './Modal/DeleteCardModal'
-import { CARD_CLASS, EVENT, CLASS_NAME, COLUMN_CLASS } from '../utils/Constants'
+import { CARD_CLASS, EVENT, COLUMN_CLASS } from '../utils/Constants'
 
 export default class Card {
   constructor(emitter, cardId, cardTitle, username, columnIndex) {
@@ -15,214 +15,214 @@ export default class Card {
 
     this.init()
   }
-}
 
-Card.prototype.init = function () {
-  this.setElements()
-  this.bindEvent()
-}
+  init() {
+    this.setElements()
+    this.bindEvent()
+  }
 
-Card.prototype.setElements = function () {
-  const template = `
-    <div class='card'>
-      <img class='document-icon' src='/static/images/document.svg') />
-      <div class='content-container'>
-        <div class='content-wrapper'>
-          <div class='title'>${this.cardTitle}</div>
-          <div class='added-by'>
-            <span>Added by </span>
-            <span class='strong'>${this.username}</span>
+  setElements() {
+    const template = `
+      <div class='card'>
+        <img class='document-icon' src='/static/images/document.svg') />
+        <div class='content-container'>
+          <div class='content-wrapper'>
+            <div class='title'>${this.cardTitle}</div>
+            <div class='added-by'>
+              <span>Added by </span>
+              <span class='strong'>${this.username}</span>
+            </div>
           </div>
-        </div>
-        <img class='${CARD_CLASS.REMOVE_BTN}' src='/static/images/remove-btn.svg' />
-        </div>
-    </div>
-  `
+          <img class='${CARD_CLASS.REMOVE_BTN}' src='/static/images/remove-btn.svg' />
+          </div>
+      </div>
+    `
 
-  this.$target = templateToElement(template)
-  this.$removeBtn = this.$target.querySelector(`.${CARD_CLASS.REMOVE_BTN}`)
-}
+    this.$target = templateToElement(template)
+    this.$removeBtn = this.$target.querySelector(`.${CARD_CLASS.REMOVE_BTN}`)
+  }
 
-Card.prototype.getTarget = function () {
-  return this.$target
-}
+  getTarget() {
+    return this.$target
+  }
 
-Card.prototype.bindEvent = function () {
-  this.$removeBtn.addEventListener('click', () => {
-    this.removeCard()
-  })
-  // this.$target.addEventListener('dblclick', this.editCard.bind(this))
-  // this.$target.ondblclick = (event) => {
-  //   event.stopPropagation()
-  //   this.editCard()
-  // }
-  var clicks = 0
-  var delay = 400
-  this.$target.addEventListener('pointerdown', e2.bind(this))
+  bindEvent() {
+    this.$removeBtn.addEventListener('click', () => {
+      this.removeCard()
+    })
+    // this.$target.addEventListener('dblclick', this.editCard.bind(this))
+    // this.$target.ondblclick = (event) => {
+    //   event.stopPropagation()
+    //   this.editCard()
+    // }
+    var clicks = 0
+    var delay = 400
+    this.$target.addEventListener('pointerdown', e2.bind(this))
 
-  function e2(e) {
-    // event.stopPropagation()
-    clicks++
+    function e2(e) {
+      // event.stopPropagation()
+      clicks++
 
-    setTimeout(function () {
-      clicks = 0
-    }, delay)
+      setTimeout(function () {
+        clicks = 0
+      }, delay)
 
-    if (clicks === 2) {
-      this.editCard()
-      console.log('더블클릭')
-      clicks = 0
-      return
-    } else {
-      this.onDragStartHandler(e)
-      console.log('포인터다운')
+      if (clicks === 2) {
+        this.editCard()
+        console.log('더블클릭')
+        clicks = 0
+        return
+      } else {
+        this.onDragStartHandler(e)
+        console.log('포인터다운')
+      }
     }
   }
-}
 
-Card.prototype.editCard = function () {
-  const modal = new EditCardModal(
-    this.cardTitle,
-    function edited(text) {
-      this.cardTitle = text
-      this.render()
-    }.bind(this)
-  )
-  modal.showModal()
-}
-
-Card.prototype.removeCard = function () {
-  const modal = new DeleteCardModal(
-    this.cardTitle,
-    function deleted() {
-      this.$target.remove()
-      this.emitter.emit(EVENT.REMOVE_CARD, this.id)
-    }.bind(this)
-  )
-  modal.showModal()
-}
-
-Card.prototype.render = function () {
-  this.$target.querySelector('.title').innerText = this.cardTitle
-}
-
-Card.prototype.onDragStartHandler = function (e) {
-  if (
-    e.target.classList.contains(CARD_CLASS.TITLE) ||
-    e.target.classList.contains(CARD_CLASS.REMOVE_BTN)
-  ) {
-    return
+  editCard() {
+    const modal = new EditCardModal(
+      this.cardTitle,
+      function edited(text) {
+        this.cardTitle = text
+        this.render()
+      }.bind(this)
+    )
+    modal.showModal()
   }
 
-  this.gapWidth = this.$target.offsetWidth / 2
-  this.gapHeight = this.$target.offsetHeight / 2
-  this.$copyTarget = this.$target.cloneNode(true)
+  removeCard() {
+    const modal = new DeleteCardModal(
+      this.cardTitle,
+      function deleted() {
+        this.$target.remove()
+        this.emitter.emit(EVENT.REMOVE_CARD, this.id)
+      }.bind(this)
+    )
+    modal.showModal()
+  }
 
-  this.$copyTarget.style.position = 'absolute'
-  this.$copyTarget.style.top = `${e.clientY - this.gapHeight}px`
-  this.$copyTarget.style.left = `${e.clientX - this.gapWidth}px`
-  this.$copyTarget.style.width = `${this.$target.offsetWidth}px`
+  render() {
+    this.$target.querySelector('.title').innerText = this.cardTitle
+  }
 
-  const $columns = Array.from(
-    document.querySelectorAll(`.${COLUMN_CLASS.COLUMN}`)
-  )
+  onDragStartHandler(e) {
+    if (
+      e.target.classList.contains(CARD_CLASS.TITLE) ||
+      e.target.classList.contains(CARD_CLASS.REMOVE_BTN)
+    ) {
+      return
+    }
 
-  this.columnDatas = $columns.map(($column) => {
-    const $cards = Array.from($column.querySelectorAll(`.${CARD_CLASS.CARD}`))
-    const cardDatas = $cards.map(($card) => {
+    this.gapWidth = this.$target.offsetWidth / 2
+    this.gapHeight = this.$target.offsetHeight / 2
+    this.$copyTarget = this.$target.cloneNode(true)
+
+    this.$copyTarget.style.position = 'absolute'
+    this.$copyTarget.style.top = `${e.clientY - this.gapHeight}px`
+    this.$copyTarget.style.left = `${e.clientX - this.gapWidth}px`
+    this.$copyTarget.style.width = `${this.$target.offsetWidth}px`
+
+    const $columns = Array.from(
+      document.querySelectorAll(`.${COLUMN_CLASS.COLUMN}`)
+    )
+
+    this.columnDatas = $columns.map(($column) => {
+      const $cards = Array.from($column.querySelectorAll(`.${CARD_CLASS.CARD}`))
+      const cardDatas = $cards.map(($card) => {
+        return {
+          $element: $card,
+          centerOffset: $card.offsetTop + $card.offsetHeight / 2,
+        }
+      })
+
       return {
-        $element: $card,
-        centerOffset: $card.offsetTop + $card.offsetHeight / 2,
+        $element: $column,
+        centerOffset: $column.offsetLeft + $column.offsetWidth / 2,
+        cardDatas,
       }
     })
 
-    return {
-      $element: $column,
-      centerOffset: $column.offsetLeft + $column.offsetWidth / 2,
-      cardDatas,
-    }
-  })
+    const pointermoveHandler = this.onPointerMoveHandler.bind(this)
 
-  const pointermoveHandler = this.onPointerMoveHandler.bind(this)
+    window.addEventListener('pointermove', pointermoveHandler)
 
-  window.addEventListener('pointermove', pointermoveHandler)
+    this.$copyTarget.addEventListener('pointerup', (e) => {
+      window.removeEventListener('pointermove', pointermoveHandler)
 
-  this.$copyTarget.addEventListener('pointerup', (e) => {
-    window.removeEventListener('pointermove', pointermoveHandler)
+      if (this.closestCardData === undefined) {
+        this.closestColumnData.$element
+          .querySelector(`.${COLUMN_CLASS.CONTENT_CONTAINER}`)
+          .appendChild(this.$target)
 
-    if (this.closestCardData === undefined) {
-      this.closestColumnData.$element
-        .querySelector(`.${COLUMN_CLASS.CONTENT_CONTAINER}`)
-        .appendChild(this.$target)
+        this.$target.classList.remove(CARD_CLASS.MOVING)
+        this.$copyTarget.remove()
+        return
+      }
+
+      const $closestCard = this.closestCardData.$element
+      if (this.closestCardData.centerOffset < e.clientY) {
+        $closestCard.parentNode.insertBefore(
+          this.$target,
+          $closestCard.nextElementSibling
+        )
+      } else {
+        $closestCard.parentNode.insertBefore(this.$target, $closestCard)
+      }
 
       this.$target.classList.remove(CARD_CLASS.MOVING)
       this.$copyTarget.remove()
-      return
-    }
+    })
 
-    const $closestCard = this.closestCardData.$element
-    if (this.closestCardData.centerOffset < e.clientY) {
-      $closestCard.parentNode.insertBefore(
-        this.$target,
-        $closestCard.nextElementSibling
-      )
-    } else {
-      $closestCard.parentNode.insertBefore(this.$target, $closestCard)
-    }
+    this.$target.classList.add(CARD_CLASS.MOVING)
+    document.body.append(this.$copyTarget)
+  }
 
-    this.$target.classList.remove(CARD_CLASS.MOVING)
-    this.$copyTarget.remove()
-  })
+  onPointerMoveHandler(e) {
+    this.$copyTarget.style.top = `${e.clientY - this.gapHeight}px`
+    this.$copyTarget.style.left = `${e.clientX - this.gapWidth}px`
 
-  this.$target.classList.add(CARD_CLASS.MOVING)
-  document.body.append(this.$copyTarget)
-}
+    this.findClosestColumn(e)
+  }
 
-Card.prototype.onPointerMoveHandler = function (e) {
-  this.$copyTarget.style.top = `${e.clientY - this.gapHeight}px`
-  this.$copyTarget.style.left = `${e.clientX - this.gapWidth}px`
+  findClosestColumn(e) {
+    ;[this.closestColumnData] = this.columnDatas.reduce(
+      ([closestColumnData, minGap], columnData) => {
+        const gap = Math.abs(columnData.centerOffset - e.clientX)
 
-  this.findClosestColumn(e)
-}
+        if (minGap === -1) {
+          return [columnData, gap]
+        }
 
-Card.prototype.findClosestColumn = function (e) {
-  ;[this.closestColumnData] = this.columnDatas.reduce(
-    ([closestColumnData, minGap], columnData) => {
-      const gap = Math.abs(columnData.centerOffset - e.clientX)
+        if (gap < minGap) {
+          closestColumnData = columnData
+          minGap = gap
+        }
 
-      if (minGap === -1) {
-        return [columnData, gap]
-      }
+        return [closestColumnData, minGap]
+      },
+      [, -1]
+    )
 
-      if (gap < minGap) {
-        closestColumnData = columnData
-        minGap = gap
-      }
+    this.findClosestCard(e, this.closestColumnData.cardDatas)
+  }
 
-      return [closestColumnData, minGap]
-    },
-    [, -1]
-  )
+  findClosestCard(e, cardDatas) {
+    ;[this.closestCardData] = cardDatas.reduce(
+      ([closestCardData, minGap], cardData) => {
+        const gap = Math.abs(cardData.centerOffset - e.clientY)
 
-  this.findClosestCard(e, this.closestColumnData.cardDatas)
-}
+        if (minGap === -1) {
+          return [cardData, gap]
+        }
 
-Card.prototype.findClosestCard = function (e, cardDatas) {
-  ;[this.closestCardData] = cardDatas.reduce(
-    ([closestCardData, minGap], cardData) => {
-      const gap = Math.abs(cardData.centerOffset - e.clientY)
+        if (gap < minGap) {
+          closestCardData = cardData
+          minGap = gap
+        }
 
-      if (minGap === -1) {
-        return [cardData, gap]
-      }
-
-      if (gap < minGap) {
-        closestCardData = cardData
-        minGap = gap
-      }
-
-      return [closestCardData, minGap]
-    },
-    [, -1]
-  )
+        return [closestCardData, minGap]
+      },
+      [, -1]
+    )
+  }
 }
