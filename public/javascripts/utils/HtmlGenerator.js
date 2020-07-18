999b65a54fdaf32139c420803500f8516655ad65
Choibumsu <bumsu0211@gmail.com>
2020-07-18 17:15:25 +0900

    [#64] refactor(utils) - 불필요한 유틸 함수 제거

--- public/javascripts/utils/HtmlGenerator.js
+++ public/javascripts/utils/HtmlGenerator.js
@@ -3,13 +3,3 @@ export const templateToElement = (template) => {
 
   return parser.parseFromString(template, 'text/html').body.firstElementChild
 }
-
-export const elementToTemplate = (element) => {
-  const tempDiv = document.createElement('div')
-  tempDiv.appendChild(element)
-
-  const template = tempDiv.innerHTML
-  tempDiv.remove()
-
-  return template
-}
