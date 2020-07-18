82c3fd63088923c5cc9bdfc518644fa04d88c3f5
Choibumsu <bumsu0211@gmail.com>
2020-07-19 04:10:43 +0900

    [#64] feat(card) - drag&drop 기능 구현 완료
    복제된 카드를 내에서 카드 간격, 컨테이너 간격의 2개의 포인트를 검사
    2개 포인트에서 가까운 카드가 있으면, 그 카드의 앞이나 뒤에 삽입
    카드가 없고 컨테이너가 있으면, 그 컨테이너에 삽입

--- public/javascripts/components/Card.js
+++ public/javascripts/components/Card.js
@@ -1,6 +1,6 @@
 import { templateToElement } from '../utils/HtmlGenerator'
 import '../../stylesheets/components/card.scss'
-import { CARD_CLASS, CLASS_NAME } from '../utils/Constants'
+import { CARD_CLASS, CLASS_NAME, COLUMN_CLASS } from '../utils/Constants'
 
 export default class Card {
   constructor({ id, title, username, nextCardId }) {
@@ -9,6 +9,8 @@ export default class Card {
     this.title = title
     this.username = username
     this.nextCardId = nextCardId
+    this.containerHalfGap = 0
+    this.cardHalfGap = 0
 
     this.init()
   }
@@ -43,17 +45,58 @@ export default class Card {
   }
 
   moveStart(e) {
+    this.setContainerHalfGap()
+    this.setCardHalfGap()
     this.copyTarget(e)
-    this.moveCopyFunc = this.moveCopy.bind(this)
+    this.moveNodesFunc = this.moveNodes.bind(this)
     this.moveStopFunc = this.moveStop.bind(this)
 
-    window.addEventListener('pointermove', this.moveCopyFunc)
+    window.addEventListener('pointermove', this.moveNodesFunc)
     window.addEventListener('pointerup', this.moveStopFunc)
+  }
+
+  setContainerHalfGap() {
+    const $firstContainer = document.querySelector(
+      `.${COLUMN_CLASS.CONTENT_CONTAINER}`
+    )
+    if (!$firstContainer) return
+
+    const $secondContainer = $firstContainer
+      .closest(`.${COLUMN_CLASS.COLUMN}`)
+      .nextElementSibling.querySelector(`.${COLUMN_CLASS.CONTENT_CONTAINER}`)
+    if (!$secondContainer) return
+
+    this.columnHalfGap =
+      ($secondContainer.offsetLeft -
+        $firstContainer.offsetLeft -
+        $firstContainer.offsetWidth) /
+      2
+  }
+
+  setCardHalfGap() {
+    const $containers = Array.from(
+      document.querySelectorAll(`.${COLUMN_CLASS.CONTENT_CONTAINER}`)
+    )
+    const twoCardsContainer = $containers.find(
+      ($container) =>
+        $container.querySelectorAll(`.${CARD_CLASS.CARD}`).length > 1
+    )
+
+    if (!twoCardsContainer) return
+
+    const $firstCard = twoCardsContainer.querySelector(`.${CARD_CLASS.CARD}`)
+    const $secondCard = $firstCard.nextElementSibling
+
+    this.cardHalfGap =
+      ($secondCard.offsetTop - $firstCard.offsetTop - $firstCard.offsetHeight) /
+      2
   }
 
   copyTarget(e) {
     this.$copyTarget = this.$target.cloneNode(true)
     this.$copyTarget.style.width = `${this.$target.offsetWidth}px`
+    this.$copyTarget.style.pointerEvents = 'none'
+    this.$copyTarget.style.opacity = '0.8'
     this.$copyTarget.style.position = 'absolute'
     this.$copyTarget.style.top = `${this.$target.offsetTop}px`
     this.$copyTarget.style.left = `${this.$target.offsetLeft}px`
@@ -66,6 +109,72 @@ export default class Card {
     document.body.appendChild(this.$copyTarget)
   }
 
+  moveNodes(e) {
+    const $points = this.setPoints()
+    this.moveTarget($points)
+    this.moveCopy(e)
+  }
+
+  setPoints() {
+    const $firstPoint = document.elementFromPoint(
+      this.$copyTarget.offsetLeft +
+        this.$copyTarget.offsetWidth / 2 -
+        this.containerHalfGap,
+      this.$copyTarget.offsetTop +
+        this.$copyTarget.offsetHeight / 2 -
+        this.cardHalfGap
+    )
+
+    const $secondPoint = document.elementFromPoint(
+      this.$copyTarget.offsetLeft +
+        this.$copyTarget.offsetWidth / 2 +
+        this.containerHalfGap,
+      this.$copyTarget.offsetTop +
+        this.$copyTarget.offsetHeight / 2 +
+        this.cardHalfGap
+    )
+
+    return [$firstPoint, $secondPoint]
+  }
+
+  moveTarget($points) {
+    if ($points.filter(($point) => !$point).length > 0) return
+
+    for (const $point of $points) {
+      const $closestCard = $point.closest(`.${CARD_CLASS.CARD}`)
+      if ($closestCard) {
+        this.insertAtCard($closestCard)
+        return
+      }
+    }
+
+    for (const $point of $points) {
+      const $closestContainer = $point.closest(
+        `.${COLUMN_CLASS.CONTENT_CONTAINER}`
+      )
+      if ($closestContainer) {
+        $closestContainer.appendChild(this.$target)
+        return
+      }
+    }
+  }
+
+  insertAtCard($originCard) {
+    if (+$originCard.dataset.id === this.id) {
+      return
+    }
+
+    if ($originCard.offsetTop < this.$copyTarget.offsetTop) {
+      $originCard.parentNode.insertBefore(
+        this.$target,
+        $originCard.nextElementSibling
+      )
+      return
+    }
+
+    $originCard.parentNode.insertBefore(this.$target, $originCard)
+  }
+
   moveCopy(e) {
     this.$copyTarget.style.top = `${e.pageY - this.offsetDiff.top}px`
     this.$copyTarget.style.left = `${e.pageX - this.offsetDiff.left}px`
@@ -75,7 +184,7 @@ export default class Card {
     this.$copyTarget.remove()
     this.$target.classList.remove(CARD_CLASS.MOVING)
     document.body.classList.remove(CLASS_NAME.US_NONE)
-    window.removeEventListener('pointermove', this.moveCopyFunc)
+    window.removeEventListener('pointermove', this.moveNodesFunc)
     window.removeEventListener('pointerup', this.moveStopFunc)
   }
 
