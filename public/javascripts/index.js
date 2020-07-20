2190e46dfb7bccd10d822825f3c51b5f02c9db38
nohgijin <ynda11111@naver.com>
2020-07-21 02:33:50 +0900

    [#84] bug(initRender) - 오류 수정

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -9,19 +9,20 @@ async function getColumn() {
   try {
     let allColumn = await fetchColumn()
     let allCard = await fetchCard()
+    allColumn.forEach((column) => {
+      column.cardDatas = []
+      allCard.forEach((card) => {
+        if (card.column_id == column.id) {
+          column.cardDatas.push(card)
+        }
+      })
+      new Column(column)
+    })
   } catch (e) {
     console.log(e)
   }
 
-  allColumn.forEach((column) => {
-    column.cardDatas = []
-    allCard.forEach((card) => {
-      if (card.column_id == column.id) {
-        column.cardDatas.push(card)
-      }
-    })
-    new Column(column)
-  })
+  
 }
 
 getColumn()
