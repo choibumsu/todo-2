504f3d5e930b859ad67db84a2faf4ec51900aecc
Choibumsu <bumsu0211@gmail.com>
2020-07-24 01:51:14 +0900

    [#135] feat(header) - header 컴포넌트 분리
    header 컴포넌트트 pug에서 분리하고 스타일 작업 진행

--- public/javascripts/components/Header.js
+++ public/javascripts/components/Header.js
@@ -1,1 +1,45 @@
 import '../../stylesheets/components/header.scss'
+
+import { CLASS_NAME, LOGIN_FORM_CLASS } from '../utils/Constants'
+import { templateToElement } from '../utils/HtmlGenerator'
+
+export default class Header {
+  constructor({ name }) {
+    this.$target = ''
+    this.username = name
+
+    this.init()
+  }
+
+  async init() {
+    this.setElements()
+    this.render()
+    this.bindEvent()
+  }
+
+  setElements() {
+    const template = `
+      <header id='page-header'>
+        <div class='left-side'>
+          <div class='title'>TODO List</div>
+          <div class='username-wrapper'>
+            <div class='username'>${this.username}</div>
+            <div>님</div>
+          </div>
+        </div>
+        <div class='right-side'>
+          <div class='logout-btn'>LOGOUT</div>
+          <div class='menu'>MENU</div>
+        </div>
+      </header>
+    `
+
+    this.$target = templateToElement(template)
+  }
+
+  render() {
+    document.body.appendChild(this.$target)
+  }
+
+  bindEvent() {}
+}
--- public/javascripts/components/SideBar.js
+++ public/javascripts/components/SideBar.js
@@ -1,14 +1,14 @@
 import { CLASS_NAME } from '../utils/Constants.js'
 import '../../stylesheets/components/sidebar.scss'
 
-const menu = document.querySelector('#menu')
-const sidebar = document.querySelector('#sidebar')
-menu.addEventListener('click', toggleSidebar)
+// const menu = document.querySelector('#menu')
+// const sidebar = document.querySelector('#sidebar')
+// menu.addEventListener('click', toggleSidebar)
 
-export function toggleSidebar() {
-  if (sidebar.classList.contains(CLASS_NAME.DP_NONE)) {
-    sidebar.classList.remove(CLASS_NAME.DP_NONE)
-    return
-  }
-  sidebar.classList.add(CLASS_NAME.DP_NONE)
-}
+// export function toggleSidebar() {
+//   if (sidebar.classList.contains(CLASS_NAME.DP_NONE)) {
+//     sidebar.classList.remove(CLASS_NAME.DP_NONE)
+//     return
+//   }
+//   sidebar.classList.add(CLASS_NAME.DP_NONE)
+// }
