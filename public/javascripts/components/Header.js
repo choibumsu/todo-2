02deccf8b1eaab3f8f6b84041726f9d62ad086c4
Choibumsu <bumsu0211@gmail.com>
2020-07-24 02:27:50 +0900

    [#135] feat(header) - sidebar 토글 함수 구현

--- public/javascripts/components/Header.js
+++ public/javascripts/components/Header.js
@@ -1,6 +1,6 @@
 import '../../stylesheets/components/header.scss'
 
-import { CLASS_NAME, LOGIN_FORM_CLASS } from '../utils/Constants'
+import { CLASS_NAME, HEADER_CLASS, SIDEBAR_CLASS } from '../utils/Constants'
 import { templateToElement } from '../utils/HtmlGenerator'
 
 export default class Header {
@@ -23,23 +23,36 @@ export default class Header {
         <div class='left-side'>
           <div class='title'>TODO List</div>
           <div class='username-wrapper'>
-            <div class='username'>${this.username}</div>
+            <div class='${HEADER_CLASS.USERNAME}'>${this.username}</div>
             <div>님</div>
           </div>
         </div>
         <div class='right-side'>
-          <div class='logout-btn'>LOGOUT</div>
-          <div class='menu'>MENU</div>
+          <div class='${HEADER_CLASS.LOGOUT_BTN}'>LOGOUT</div>
+          <div class='${HEADER_CLASS.MENU}'>MENU</div>
         </div>
       </header>
     `
 
     this.$target = templateToElement(template)
+    this.$menuBtn = this.$target.querySelector(`.${HEADER_CLASS.MENU}`)
   }
 
   render() {
     document.body.appendChild(this.$target)
   }
 
-  bindEvent() {}
+  bindEvent() {
+    this.$menuBtn.addEventListener('click', this.toggleMenu)
+  }
+
+  toggleMenu() {
+    const $sidebar = document.querySelector(`.${SIDEBAR_CLASS.SIDEBAR}`)
+
+    if ($sidebar.classList.contains(CLASS_NAME.DP_NONE)) {
+      $sidebar.classList.remove(CLASS_NAME.DP_NONE)
+      return
+    }
+    $sidebar.classList.add(CLASS_NAME.DP_NONE)
+  }
 }
